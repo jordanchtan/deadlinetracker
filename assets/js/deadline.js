2c56f3010bdff7a67bd1afed2240b46343de12a0
Jordan Tan <Jordan Tan>
2017-10-15 13:15:14 +0100

    Added styling and functionality

--- assets/js/deadline.js
+++ assets/js/deadline.js
@@ -1,15 +1,22 @@
-// Mark completion by clicking
-$("li").click(function() {
-  if($(this).css("color") === "rgb(128, 128, 128)") {
-    $(this).css({
-      color: "black",
-      textDecoration: "none"
-    });
-  } else {
-    $(this).css({
-      color: "gray",
-      textDecoration: "line-through"
-    });
+$("input[type='text']").keypress(function(event) {
+  if (event.which === 13) {
+    var deadlineText = $(this).val();
+    $(this).val("");
+    $("ul").append("<li><span><i class='fa fa-times' aria-hidden='true'></i></span>" + deadlineText + "</li>");
   }
+})
 
+
+
+
+$("ul").on("click", "li", function() {
+  $(this).toggleClass("completed");
+})
+
+$("ul").on("click", "span", function() {
+  $(this).parent().fadeOut(500, function() {
+    $(this).remove();
+  });
+  $(this).parent().remove();
+  event.stopPropagation();
 })

590206df1cc790d55adecf23e0fbca43e5d0eac7
Jordan Tan <Jordan Tan>
2017-10-15 13:25:36 +0100

    Added toggle functionality

--- assets/js/deadline.js
+++ assets/js/deadline.js
@@ -4,19 +4,22 @@ $("input[type='text']").keypress(function(event) {
     $(this).val("");
     $("ul").append("<li><span><i class='fa fa-times' aria-hidden='true'></i></span>" + deadlineText + "</li>");
   }
-})
+});
 
 
 
 
 $("ul").on("click", "li", function() {
   $(this).toggleClass("completed");
-})
+});
 
-$("ul").on("click", "span", function() {
+$("ul").on("click", "span", function(event) {
   $(this).parent().fadeOut(500, function() {
     $(this).remove();
   });
-  $(this).parent().remove();
   event.stopPropagation();
-})
+});
+
+$(".fa-plus").click(function() {
+  $("input[type='text']").fadeToggle()
+});
